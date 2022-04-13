235c7eaf1e301b1f9c1342163fbda56461cfa2d5
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-04-13 17:30:35 -0300

    creating project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Routers } from "./routes/Routers"
+import { GlobalStyle } from "./styles/global"
 
 export const App = () => {
   return (
-   <></>
+    <>
+      <Routers />
+      <GlobalStyle />
+    </>
   )
 }
 
